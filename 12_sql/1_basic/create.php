240033797f976a6499979d73ac56cbd33be3416a
heinhtetzan <mmsonestopit@gmail.com>
2020-01-31 14:07:50 +0630

    today

--- 12_sql/1_basic/create.php
+++ 12_sql/1_basic/create.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Create</title>
     <style>
         fieldset{
             width: 300px;
@@ -20,8 +20,39 @@
     </style>
 </head>
 <body>
+<pre>
+<?php
 
-<form action="">
+    if(isset($_GET['create'])){
+        //get
+//        print_r($_GET);
+        $name = $_GET["name"];
+        $phone = $_GET["phone"];
+
+        //mysql connection
+        $con = mysqli_connect("localhost","root","","weekday_sql");
+
+        if($name && $phone){
+
+            $sql = "INSERT INTO contacts (name,phone) VALUES ('$name','$phone')";
+
+//            $query = mysqli_query($con,$sql);
+
+            if(mysqli_query($con,$sql)){
+                header("location:create.php");
+            }else{
+                echo "db input fail";
+
+            }
+
+
+        }
+//        header("location:create.php");
+    }
+
+?>
+</pre>
+<form action="" method="get">
 
     <fieldset>
         <legend>Contact Create Form</legend>
---
+++ 12_sql/1_basic/read.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Read</title>
+    <style>
+        table{
+            width: 100%;
+        }
+        table,tr,th,td{
+            border:1px solid black;
+            border-collapse: collapse;
+            padding: 10px 15px;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <thead>
+    <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Phone</th>
+        <th>Controls</th>
+        <th>Reg Time</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Hein Htet Zan</td>
+        <td>09623832848</td>
+        <td>
+
+        </td>
+        <td>
+            12 June 2020
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
